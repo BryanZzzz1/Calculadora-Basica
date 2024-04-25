d7bd1efb7da2f9bc7995f1fedb9b62583968cbd2
unknown <bryansilvamedina@gmail.com>
2024-04-25 16:56:42 -0400

    calculadora incompleta

---
+++ assets/js/scripts.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+    let numero1 = 0;
+    let numero2 = 0;
+    let resultado = 0;
+
+    const regex = /^\d+$/;
+
+    $("#btnsumar").click(function(){
+
+        $("#error1").text("");
+        $("#error2").text("");
+        $("#resultado").text("");
+
+        numero1 = $("#numero1").val();
+        if(numero1 === ""){
+            $("#error1").text("Ingrese un número adecuado");
+            $("#error1").css("color","red");
+            return;
+        }
+        if(!regex.test(numero1)){
+            $("#error1").text("Error: ingrese un número adecuado");
+            $("#error1").css("color","red");
+            return;
+        }
+
+        numero2 = $("#numero2").val();
+        if(numero2 === ""){
+            $("#error2").text("Ingrese un número adecuado");
+            $("#error2").css("color","red");
+            return;
+        }
+        if(!regex.test(numero2)){
+            $("#error2").text("Error: ingrese un número adecuado");
+            $("#error2").css("color","red");
+            return;
+        }
+
+        resultado = parseInt(numero1) + parseInt(numero2);
+        $("#resultado").text(resultado);
+        $("#resultado").css("color","blue");
+        $("#resultado").css("font-size","2rem");
+
+    });
+});
