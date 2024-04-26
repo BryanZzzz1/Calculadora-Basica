713f2286da138ffcceea3b14935ce6c892bc3cfe
BryanX19xzzzzz <bryansilvamedina@gmail.com>
2024-04-25 21:43:53 -0400

    terminus

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -42,4 +42,117 @@ $(document).ready(function(){
         $("#resultado").css("font-size","2rem");
 
     });
+    $("#btnrestar").click(function(){
+
+        $("#error1").text("");
+        $("#error2").text("");
+        $("#resultado").text("");
+
+        numero1 = $("#numero1").val();
+        if(numero1 === ""){
+            $("#error1").text("Ingrese un número adecuado");
+            $("#error1").css("color","red");
+            return;
+        }
+        if(!regex.test(numero1)){
+            $("#error1").text("Error: ingrese un número adecuado");
+            $("#error1").css("color","red");
+            return;
+        }
+
+        numero2 = $("#numero2").val();
+        if(numero2 === ""){
+            $("#error2").text("Ingrese un número adecuado");
+            $("#error2").css("color","red");
+            return;
+        }
+        if(!regex.test(numero2)){
+            $("#error2").text("Error: ingrese un número adecuado");
+            $("#error2").css("color","red");
+            return;
+        }
+
+        resultado = parseInt(numero1) - parseInt(numero2);
+        $("#resultado").text(resultado);
+        $("#resultado").css("color","blue");
+        $("#resultado").css("font-size","2rem");
+
+    });
+    $("#btndividir").click(function(){
+
+        $("#error1").text("");
+        $("#error2").text("");
+        $("#resultado").text("");
+
+        numero1 = $("#numero1").val();
+        if(numero1 === ""){
+            $("#error1").text("Ingrese un número adecuado");
+            $("#error1").css("color","red");
+            return;
+        }
+        if(!regex.test(numero1)){
+            $("#error1").text("Error: ingrese un número adecuado");
+            $("#error1").css("color","red");
+            return;
+        }
+
+        numero2 = $("#numero2").val();
+        if(numero2 === ""){
+            $("#error2").text("Ingrese un número adecuado");
+            $("#error2").css("color","red");
+            return;
+        }
+        if(!regex.test(numero2)){
+            $("#error2").text("Error: ingrese un número adecuado");
+            $("#error2").css("color","red");
+            return;
+        }
+
+        resultado = parseInt(numero1) / parseInt(numero2);
+        $("#resultado").text(resultado);
+        $("#resultado").css("color","blue");
+        $("#resultado").css("font-size","2rem");
+        
+    });
+    $("#btnmulti").click(function(){
+
+        $("#error1").text("");
+        $("#error2").text("");
+        $("#resultado").text("");
+
+        numero1 = $("#numero1").val();
+        if(numero1 === ""){
+            $("#error1").text("Ingrese un número adecuado");
+            $("#error1").css("color","red");
+            return;
+        }
+        if(!regex.test(numero1)){
+            $("#error1").text("Error: ingrese un número adecuado");
+            $("#error1").css("color","red");
+            return;
+        }
+
+        numero2 = $("#numero2").val();
+        if(numero2 === ""){
+            $("#error2").text("Ingrese un número adecuado");
+            $("#error2").css("color","red");
+            return;
+        }
+        if(!regex.test(numero2)){
+            $("#error2").text("Error: ingrese un número adecuado");
+            $("#error2").css("color","red");
+            return;
+        }
+
+        resultado = parseInt(numero1) * parseInt(numero2);
+        $("#resultado").text(resultado);
+        $("#resultado").css("color","blue");
+        $("#resultado").css("font-size","2rem");   
+    });
+    $("#btnreset").click(function(){ 
+        numero1 = $("#numero1").reset(); 
+        numero2 = $("#numero2").reset();
+        
+    });
+
 });
